28e5f825a5a613a4286d8a74e1546f9569dfb326
Josemi <josemijuanes@gmail.com>
2022-01-03 21:04:36 +0100

    Improve css syntax highlight

--- highlight.js
+++ highlight.js
@@ -8,18 +8,17 @@ const tokenTypes = [
     "regex",             // 5: regex expressions
     "number",            // 6: numbers
     "tag",               // 7: XML tagnames
-    "attribute.name",    // 8: XML attribute names
-    "attribute.value",   // 9: XML attribute values
-    "selector",          // 10: CSS selectors
-    "flag",              // 11: CSS flags (!improtant, !default)
-    "string",            // 12: double quote string ("...")
-    "string",            // 13: single quote string ('...')
-    "string",            // 14: backtick quoted strings (`...`)
-    "comment",           // 15: XML comment (<!-- ... -->)
-    "comment",           // 16: inline comment (//....)
-    "comment",           // 17: block comment (/* ... */)
-    "unit",              // 18: CSS unit (px, rem, em, %)
-    "color",             // 19: CSS color (#000, #0000)
+    "attribute",         // 8: XML attribute names
+    "selector",          // 9: CSS selector
+    "property",          // 10: CSS property
+    "unit",              // 11: CSS unit value
+    "function",          // 12: JS function
+    "string",            // 13: double quote string ("...")
+    "string",            // 14: single quote string ('...')
+    "string",            // 15: backtick quoted strings (`...`)
+    "comment",           // 16: XML comment (<!-- ... -->)
+    "comment",           // 17: inline comment (//....)
+    "comment",           // 18: block comment (/* ... */)
 ];
 
 // Javascript keywords
@@ -28,32 +27,41 @@ const jsKeywords = [
     "debugger","default","delete","do","else","export","extends","finally","for","from",
     "function","if","implements","import","in","instanceof","interface","let","module","new",
     "of","return","static","super","switch","symbol","this","throw",
-    "try","typeof","undefined","var","void","while","with","yield"
+    "try","typeof","undefined","var","void","while","with","yield",
 ];
 const jsConstants = ["null", "true", "false"];
 
-// CSS/SCSS keywords
+// CSS/SCSS keywords and constants
 const cssKeywords = [
+    "@font-face", "@import", "@keyframes",
+];
+const cssConstants = [
     "absolute", "relative", "fixed", "sticky",
     "bold", "normal",
     "auto", "none",
     "sans-serif", "sans", "serif", "monospace",
+    "red", "white", "black", "blue", "yellow", "green", "orange", "gray",
 ];
 
 //Global regular expressions
 const regexp = {
     "nonWhitespace": /[\S]/,
     "word": /[\w]/,
+    "keyword": /[\w\-@]/,
     "number": /[\d\.]/,
+    "punctuation": /^[{}[\](\):;\\.,]/,
+    "operator": /^[?!&@~\/\-+*%=<>|]/,
+    "htmlTag": /^[\w\d\-\_]/,
     "htmlPunctuation": /^[\/=<>]/,
-    "htmlTag": /^[\w\d\.\-\_]/,
     "htmlAttribute": /^([\w\d\.\-\_]+)\=/,
-    "jsPunctuation": /^[{}[\](\):;\\.,]/,
-    "jsOperator": /^[?!&@~\/\-+*%=<>|]/,
     "jsKeyword": new RegExp(`^(${jsKeywords.join("|")})$`, ""),
     "jsConstant": new RegExp(`^(${jsConstants.join("|")})[^\w]`, ""),
-    "cssKeyword": new RegExp(`^(${cssKeywords.join("|")})$`, ""),
-    "cssUnit": /^[\d\.]+\s*(cm|mm|in|px|pt|pc|em|rem|vw|vh|%)/,
+    "jsFunction": /^([a-zA-Z][\w]*)\ *\(/,
+    "cssKeyword": new RegExp(`^(${cssKeywords.join("|")})`, ""),
+    "cssConstant": new RegExp(`^(${cssConstants.join("|")})[^\w]`, ""),
+    "cssSelector": /^\.([\w\d\-\_]+)/,
+    "cssProperty": /^([\w\d\.\-\_]+)\ *\:/,
+    "cssUnit": /^(cm|mm|in|px|pt|pc|em|rem|vw|vh)/,
     "cssColor": /^#([\da-f]{3}){1,2}/,
     "doubleQuote": /[\"]/,
     "singleQuote": /[\']/,
@@ -93,9 +101,7 @@ const isKeyword = (value, lang) => {
 };
 
 //Check if is comment token
-const isCommentToken = index => {
-    return 15 <= index && index <= 17;
-};
+const isCommentToken = index => 12 <= index && index <= 14;
 
 //Create a token
 const createToken = t => {
@@ -117,31 +123,29 @@ const renderToken = ctx => {
 const shouldEndToken = ctx => {
     const c = ctx.current;
     const {type, content} = ctx.token;
-    //Check for whitespace or word tokens
+    // Check for whitespace or word tokens
     if (type === 0) { 
         return regexp.nonWhitespace.test(c) || !regexp.word.test(c); //Check for no word
     }
     else if (type === 1 || type === 2) { return true; } //Always end a punctuation or operator sign
-    //TODO: check for closing regex tokens
-    else if (type === 3 || type === 4) { return !regexp.word.test(c); } //End of word
+    // TODO: check for closing regex tokens
+    else if (type === 3) { return !regexp.keyword.test(c); } // End of keyword
+    else if (type === 4) { return !regexp.word.test(c); } //End of word
     else if (type === 6) { return !regexp.number.test(c); } //Check for no digit
     else if (type === 7 || type === 8) { return !regexp.htmlTag.test(c); } //End attribute name or tag
-    else if (type === 9 || type === 12) {
-        return content.length > 1 && regexp.doubleQuote.test(ctx.last); //End attribute value or double quote str
-    }
-    //TODO: end CSS tokens
-    //End of strings tokes
-    else if (type === 13) { return content.length > 1 && regexp.singleQuote.test(ctx.last); } //End single quotes
-    else if (type === 14) { return content.length > 1 && regexp.backtick.test(ctx.last); } //End backtick quotes
-    //End of comments tokens
-    else if (type === 15) { return content.endsWith("-->"); } //End block comment token
-    else if (type === 16) { return c === "\n"; } //End single line token
-    else if (type === 17) { return content.endsWith("*/"); } //End block comment token
-    else if (type === 18) { return regexp.cssUnit.test(content); } // End CSS unit token
-    else if (type === 19) {
-        return regexp.cssColor.test(content) && !regexp.word.test(c); // End CSS color token
-    }
-    //Other value???
+    else if (type === 9) { return c !== "." && !regexp.htmlTag.test(c); } // End CSS selector
+    else if (type === 10) { return c === ":"; } // End CSS property 
+    else if (type === 11) { return !regexp.word.test(c); } // End CSS unit token
+    else if (type === 12) { return c === "("; } // End function
+    // End strings tokes
+    else if (type === 13) { return content.length > 1 && regexp.doubleQuote.test(ctx.last); } //End attribute value or double quote str
+    else if (type === 14) { return content.length > 1 && regexp.singleQuote.test(ctx.last); } //End single quotes
+    else if (type === 15) { return content.length > 1 && regexp.backtick.test(ctx.last); } //End backtick quotes
+    // End comments tokens
+    else if (type === 16) { return content.endsWith("-->"); } //End block comment token
+    else if (type === 17) { return c === "\n"; } //End single line token
+    else if (type === 18) { return content.endsWith("*/"); } //End block comment token
+    // Other value???
     return false;
 };
 
@@ -151,10 +155,10 @@ const getNextToken = ctx => {
     const mode = ctx.mode; //Get current mode
     //HTML language tokens
     if (ctx.lang === "html") {
-        if (ctx.code.startsWith("<!--")) { return createToken(15); } //Comment token
+        if (ctx.code.startsWith("<!--")) { return createToken(16); } //Comment token
         else if (c === "<") { return createToken(2); } //HTML punctuation
         else if (mode === "tag" && regexp.htmlPunctuation.test(c)) { return createToken(2); } //HTML punctuation
-        else if (mode === "tag" && regexp.doubleQuote.test(c)) { return createToken(9); } //Attribute value
+        else if (mode === "tag" && regexp.doubleQuote.test(c)) { return createToken(13); } //Attribute value
         else if (mode === "tag" && regexp.htmlTag.test(c)) {
             //Check for tag or attribute name
             return (ctx.last === "<" || ctx.last === "/") ? createToken(7) : createToken(8);
@@ -162,27 +166,35 @@ const getNextToken = ctx => {
     }
     //Javascript language tokens
     else if (ctx.lang === "js") {
-        if (ctx.code.startsWith("/*")) { return createToken(17); } //Block comment token
-        else if (ctx.code.startsWith("//")) { return createToken(16); } //Line comment token
-        else if (regexp.backtick.test(c)) { return createToken(14); } //Backtick string
-        else if (regexp.singleQuote.test(c)) { return createToken(13); } //Single quote string
-        else if (regexp.doubleQuote.test(c)) { return createToken(12); } //Double quote string
-        else if (regexp.jsPunctuation.test(c)) { return createToken(2); } //Punctuation token
-        else if (regexp.jsOperator.test(c)) { return createToken(1); } //Operator token
+        if (ctx.code.startsWith("/*")) { return createToken(18); } //Block comment token
+        else if (ctx.code.startsWith("//")) { return createToken(17); } //Line comment token
+        else if (regexp.backtick.test(c)) { return createToken(15); } //Backtick string
+        else if (regexp.singleQuote.test(c)) { return createToken(14); } //Single quote string
+        else if (regexp.doubleQuote.test(c)) { return createToken(13); } //Double quote string
+        else if (regexp.punctuation.test(c)) { return createToken(2); } //Punctuation token
+        else if (regexp.operator.test(c)) { return createToken(1); } //Operator token
         else if (regexp.number.test(c)) { return createToken(6); } //Number
         else if (regexp.jsConstant.test(ctx.code)) { return createToken(4); } //Language constant
+        else if (regexp.jsFunction.test(ctx.code)) { return createToken(12); } // Function
         else if (regexp.word.test(c)) { return createToken(3); } //Keyword
     }
     // CSS / SCSS language tokens
     else if (ctx.lang === "css" || ctx.lang === "scss") {
-        if (ctx.code.startsWith("/*")) { return createToken(17); } //Block comment token
-        else if (ctx.code.startsWith("//")) { return createToken(16); } //Line comment token
-        else if (regexp.doubleQuote.test(c)) { return createToken(12); } //Double quote string
-        else if (regexp.singleQuote.test(c)) { return createToken(13); } //Single quote string
-        else if (regexp.cssUnit.test(ctx.code)) { return createToken(18); } // CSS Unit
-        else if (regexp.cssColor.test(ctx.code)) { return createToken(19); } // CSS Color
+        if (ctx.code.startsWith("/*")) { return createToken(18); } //Block comment token
+        else if (ctx.code.startsWith("//")) { return createToken(17); } //Line comment token
+        else if (regexp.doubleQuote.test(c)) { return createToken(13); } //Double quote string
+        else if (regexp.singleQuote.test(c)) { return createToken(14); } //Single quote string
+        else if (regexp.cssKeyword.test(ctx.code)) { return createToken(3); } //Keyword
+        else if (regexp.cssConstant.test(ctx.code)) { return createToken(4); } //Language constant
+        else if (ctx.mode !== "block" && !regexp.nonWhitespace.test(ctx.last) && regexp.htmlTag.test(ctx.code)) {
+            return createToken(7); // CSS Attribute
+        }
+        else if (ctx.mode !== "block" && regexp.cssSelector.test(ctx.code)) { return createToken(9); } // CSS attribute
+        else if (regexp.cssProperty.test(ctx.code)) { return createToken(10); } // CSS property
+        else if (regexp.cssUnit.test(ctx.code)) { return createToken(11); } // CSS Unit
+        else if (regexp.punctuation.test(c)) { return createToken(2); } //Punctuation token
+        else if (regexp.operator.test(c)) { return createToken(1); } //Operator token
         else if (regexp.number.test(c)) { return createToken(6); } //Number
-        else if (regexp.word.test(c)) { return createToken(3); } //Keyword
     }
     //Default token
     return createToken(0);
@@ -228,6 +240,10 @@ const highlightCtx = ctx => {
                 else if (ctx.token.content === "<" && ctx.code.startsWith("style")) {
                     subLanguage = "css"; // CSS sublanguage
                 }
+            }
+            else if (ctx.lang === "css") {
+                if (ctx.last === "{") { ctx.mode = "block"; }
+                else if (ctx.last === "}") { ctx.mode = null; }
             }
             //Check if we should run the code in another language
             if (shouldRunSublanguage === true && subLanguage !== null) {
